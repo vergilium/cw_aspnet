c03db0852f3fefa8d602aa0e89262ffa307d700e
vergilium <vergilium2@gmail.com>
2021-02-11 00:32:29 +0200

    Restructured project

---
+++ src/DB/DbAbstractions/DbIdentity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DbAbstractions {
+    public abstract class DbIdentity : IDbIdentity<int> {
+        public int id { get; set; }
+    }
+    public abstract class DbIdentity<TId> : IDbIdentity<TId> {
+        public virtual TId id { get; set; }
+        
+        [NotMapped]
+        public string StringId
+        {
+            get => GetStringId(id);
+            set => id = GetTypedId(value);
+        }
+
+        /// <summary>
+        /// Converts an outgoing typed resource identifier to string format.
+        /// </summary>
+        protected virtual string GetStringId(TId value)
+        {
+            return EqualityComparer<TId>.Default.Equals(value, default) ? null : value.ToString();
+        }
+
+        /// <summary>
+        /// Converts an incoming 'id' element to the typed resource identifier.
+        /// </summary>
+        protected virtual TId GetTypedId(string value)
+        {
+            return value == null ? default : (TId)Convert.ChangeType(value, typeof(TId));
+        }
+    }
+}
---
+++ src/DB/DbContext/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbContext {
+    public partial class AppDbContext : IdentityDbContext {
+        public AppDbContext(DbContextOptions options):base(options){}
+        /***
+         * DB SETs here
+         */
+    }
+}
